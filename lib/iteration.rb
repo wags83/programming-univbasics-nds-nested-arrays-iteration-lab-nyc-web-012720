89e1692071444e848c9d1cf0e18614ac44829959
Greg Wagner <gpwagner@gmail.com>
2020-01-15 21:13:39 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,7 +9,7 @@ def join_ingredients(src)
 row_index = 0
 return_array = []
 while row_index < src.count do
-    return_array << "I love #{src.row_index[0]} and #{src.row_index[1]} on my pizza"
+    return_array << 'I love #{src.row_index[0]} and #{src.row_index[1]} on my pizza'
   end
   return_array
 end
